edd3aff64448efde46d3c37a3c3c9264813e1a27
Prari2725 <pragatirpehrkar@gmail.com>
2026-01-07 16:04:23 +0530

    security origin

--- BACKEND/invoicify/src/main/java/com/invoice/config/WebSecurityConfig.java
+++ BACKEND/invoicify/src/main/java/com/invoice/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://invoicify-5nm.pages.dev"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
